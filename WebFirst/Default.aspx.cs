f70b0a4ffce719f507fe8428f76be5cebfe01f95
josem-bmgf <56959263+josem-bmgf@users.noreply.github.com>
2020-10-20 03:35:10 -0700

    saple web
    push sample web app

---
+++ WebFirst/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace WebFirst
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            WebClass wc = new WebClass();
+            txtInput.Text = wc.Showme();
+
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            WebClass wc = new WebClass();
+            GrdView.DataSource = wc.ConnectME();
+            GrdView.DataBind();
+        }
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = null;
+            
+        }
+    }
+}
---
+++ WebFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFirst.Global" Language="C#" %>
