8907297a985c507cfd66e7be21f6f7162f22b227
josem-bmgf <56959263+josem-bmgf@users.noreply.github.com>
2021-04-14 06:45:32 -0700

    Add build tools

---
+++ BuildTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
